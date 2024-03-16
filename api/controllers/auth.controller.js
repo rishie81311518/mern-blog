acd3dfde21b3a87daf97ab2ded4475a93f4b6f44
Rishi <rishiekandala@gmail.com>
2024-03-16 10:06:18 +0530

    create sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,8 @@
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -13,7 +13,7 @@ export const signup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-   next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
