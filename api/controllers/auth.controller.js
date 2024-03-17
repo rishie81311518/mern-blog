357ff837d2e126cc5a0aac50f525f94a7880fff6
Rishi <rishiekandala@gmail.com>
2024-03-17 22:16:49 +0530

    need to change sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,3 +31,12 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req,res,next) => {
+  const {email, password} = req.body;
+
+  if (!email || ! password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+}
+
